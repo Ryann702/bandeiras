f797d8d2940135f20fbf984f58b9dea51573cb9e
Ryan Monteiro <monteroryang@gmail.com>
2025-12-04 19:24:45 +0000

    Implementando o map

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
-
 import flags from './model/flags.js';
 const areaPrincipal = document.querySelector('#flags-container');
 
-for (const bandeira of flags) {
-  const layoutDaBandeira = criarLayoutDaBandeira(bandeira);
-  areaPrincipal.insertAdjacentHTML('beforeend', layoutDaBandeira);
-}
-
-function criarLayoutDaBandeira(bandeira) {
-  const layout = `
-    <div class="flag col-2 my-2 text-center">
-      <img src="${bandeira.image}" alt="${bandeira.name}">
-      <p>${bandeira.name}</p>
-    </div>
-  `;
-  return layout;
-}
+areaPrincipal.innerHTML = flags.map(bandeira => `
+  <div class="flag col-2 my-2 text-center">
+    <img src="${bandeira.image}" alt="${bandeira.name}">
+    <p>${bandeira.name}</p>
+  </div>
+`).join('');
